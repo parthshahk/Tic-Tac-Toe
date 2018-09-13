d5868f98c4858c4beb8287b401b6b99e197d22f0
Parth Shah <parthshahk@gmail.com>
2018-09-13 23:04:39 +0530

    Updated API

--- api/auth.php
+++ api/auth.php
@@ -18,7 +18,7 @@
         }
 
     }else if($action == 'signup'){
-
+        
         $username = $_REQUEST['username'];
         $password = $_REQUEST['password'];
 
@@ -29,7 +29,45 @@
 
     }else if($action == 'login'){
         
-        
+        $username = $_REQUEST['username'];
+        $password = $_REQUEST['password'];
+
+        $result = mysqli_query($con, "SELECT * FROM users WHERE Username='$username' AND Password='$password'");
+
+        if(mysqli_num_rows($result)){
+
+            session_start();
+            $_SESSION['loggedIn'] = true;
+            $_SESSION['username'] = $username;
+            echo 'success';
+            exit(0);
+
+        }else{
+            echo 'invalid';
+            exit(0);
+        }
+
+    }else if($action == 'logout'){
+
+        session_start();
+        session_destroy();
+        exit(0);
+
+    }else if($action == 'status'){
+
+        session_start();
+
+        if(isset($_SESSION['loggedIn'])){
+            if($_SESSION['loggedIn'] == true){
+
+                echo $_SESSION['username'];
+                exit(0);
+
+            }
+        }
+
+        echo 'false';
+        exit(0);
 
     }
 
