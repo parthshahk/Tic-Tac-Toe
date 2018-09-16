366cf41f53902d499a145b749f340ddb8b543e4b
Parth Shah <parthshahk@gmail.com>
2018-09-16 19:29:44 +0530

    Updated Gameplay

--- api/game.php
+++ api/game.php
@@ -7,12 +7,41 @@
     if($action == 'gameInfo'){
 
         $game['username'] = $_SESSION['username'];
+
         $game['creator'] = $_SESSION['creator'];
         $game['joiner'] = $_SESSION['joiner'];
+
         $game['id'] = $_SESSION['id'];
+
+        $game['turn'] = $_SESSION['creator'];
+
+
+        $_SESSION['state'] = array("c","c","c","x","c","c","c","o","c");
+        $game['state'] = $_SESSION['state'];
+
+
+        //Initialize database state
+        $gameStates = implode(",",$_SESSION['state']);
+        $id = $_SESSION['id'];
+        mysqli_query($con, "UPDATE session SET State='$gameStates' WHERE ID=$id");
 
         echo json_encode($game);
         exit(0);
+
+    }else if($action == 'move'){   
+        
+        $move = $_REQUEST['move'];
+        $type = $_REQUEST['type'];
+
+        if($type == 'x'){
+            $_SESSION['state'][$move] = 'x';
+        }else{
+            $_SESSION['state'][$move] = 'o';
+        }
+
+        $gameStates = implode(",",$_SESSION['state']);
+        $id = $_SESSION['id'];
+        mysqli_query($con, "UPDATE session SET State='$gameStates' WHERE ID=$id");
 
     }
 ?>
