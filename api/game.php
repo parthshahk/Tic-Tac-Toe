93f81f8562c09bf15c3fe1ef87dbef1f6fbb4ecf
Parth Shah <parthshahk@gmail.com>
2018-09-16 22:46:33 +0530

    Gameplay Updated

--- api/game.php
+++ api/game.php
@@ -13,7 +13,9 @@
 
         $game['id'] = $_SESSION['id'];
 
-        $game['turn'] = $_SESSION['creator'];
+        $_SESSION['turn'] = $_SESSION['creator'];
+        $game['turn'] = $_SESSION['turn'];
+
 
 
         $_SESSION['state'] = array("c","c","c","x","c","c","c","o","c");
@@ -32,16 +34,43 @@
         
         $move = $_REQUEST['move'];
         $type = $_REQUEST['type'];
+        $id = $_SESSION['id'];
+
 
         if($type == 'x'){
+
             $_SESSION['state'][$move] = 'x';
+            $_SESSION['turn'] = $_SESSION['joiner'];
+            $turn = $_SESSION['turn'];
+            mysqli_query($con, "UPDATE session SET Turn='$turn' WHERE ID=$id");
+
         }else{
             $_SESSION['state'][$move] = 'o';
+            $_SESSION['turn'] = $_SESSION['creator'];
+            $turn = $_SESSION['turn'];
+            mysqli_query($con, "UPDATE session SET Turn='$turn' WHERE ID=$id");
         }
 
         $gameStates = implode(",",$_SESSION['state']);
-        $id = $_SESSION['id'];
         mysqli_query($con, "UPDATE session SET State='$gameStates' WHERE ID=$id");
+
+        exit(0);
+
+    }else if($action == 'gameUpdates'){
+
+        $id = $_SESSION['id'];
+        $result = mysqli_query($con, "SELECT Turn,State FROM session WHERE ID=$id");
+        $row=mysqli_fetch_assoc($result);
+
+        $_SESSION['turn'] = $row['Turn'];
+        $game['turn'] = $_SESSION['turn'];
+
+        $gameStates = explode(",",$row['State']);
+        $_SESSION['state'] = $gameStates;
+        $game['state'] = $_SESSION['state'];
+
+        echo json_encode($game);
+        exit(0);
 
     }
 ?>
