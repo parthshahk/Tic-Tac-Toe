ced05a68c90b271077faed568e4a373647660731
Parth Shah <parthshahk@gmail.com>
2018-10-19 10:17:54 +0530

    .

--- api/connection.php
+++ api/connection.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("localhost","parth","parth","ttt");
-    // $con=mysqli_connect("mysql.hostinger.in","u696737897_parth","`7w4]8io`aR8xu1AB1","u696737897_datab");
+    // $con = mysqli_connect("localhost","parth","parth","ttt");
+    $con=mysqli_connect("mysql.hostinger.in","u696737897_parth","`7w4]8io`aR8xu1AB1","u696737897_datab");
 
 ?>
